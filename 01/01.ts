9c73c69fea38b19ac26602fd6c0802c15963941a
AntonioMrtz <antonio@metrica-sports.com>
2025-12-01 23:03:18 +0100

    style: run format

--- 01/01.ts
+++ 01/01.ts
@@ -1,6 +1,5 @@
 export { filterGifts };
 
 function filterGifts(gifts: string[]): string[] {
-  // eslint-disable-next-line quotes
-  return gifts.filter((gift) => !gift.includes("#"));
+  return gifts.filter((gift) => !gift.includes('#'));
 }
